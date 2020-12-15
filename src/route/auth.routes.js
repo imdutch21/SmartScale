99149a6508e76bd8c8c2355f87f1abb371bcb205
bartpklomp@gmail.com <Ejl2Pd0E12Ub>
2020-12-15 16:44:32 +0100

    Fixed naming error

--- src/route/auth.routes.js
+++ src/route/auth.routes.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const AuthController = require('../controller/auth.controller');
 
 router.get('/authGoogle', AuthController.authGoogle);
-router.post('/refreshToken',AuthController.refresh);
+router.post('/refreshToken',AuthController.refreshToken);
 
 module.exports = router;
