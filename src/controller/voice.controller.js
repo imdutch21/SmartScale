98a27ec5787010f4047c46417084234631d95409
imdutch21 <bartpklomp@gmail.com>
2021-01-05 16:11:29 +0100

    Added first test for parameters

--- src/controller/voice.controller.js
+++ src/controller/voice.controller.js
@@ -2,20 +2,36 @@ const ApiError = require('../model/ApiError');
 const assert = require('assert');
 
 
+
+function handleNewFoodMeasure(params) {
+
+    return {
+        prompt: {
+            override: false,
+            firstSimple: {
+                speech: `We successfully measured your ${params} at a total of 200 grams.`,
+                text: ""
+            }
+        }
+    }
+}
+
 module.exports = {
     googleAsssistent(request, response, next) {
 
         console.log(request.body);
-        response.status(200).json({
-            session: request.body.session,
-            prompt: {
-                override: false,
-                firstSimple: {
-                    speech: "Hello World.",
-                    text: ""
-                }
+        let handler = request.body.handler;
+        let responseMessage = {};
+        let params = request.body.session.params;
+        switch (handler) {
+            case "NewFoodMeasured": {
+                responseMessage = handleNewFoodMeasure(params);
+                break;
             }
-        }).end();
+        }
+
+        responseMessage.session = request.body.session;
+        response.status(200).json(responseMessage).end();
 
     }
 
